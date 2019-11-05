345a905b54cb2f055c4d6bfb6af2552b7c93b198
Mary Potempa <mappotem@iu.edu>
2019-11-05 10:35:26 -0500

    delivery buttons transition to menu screen

--- mappotem_UI/Assets/Scripts/UIManager.cs
+++ mappotem_UI/Assets/Scripts/UIManager.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public GameObject deliveryPanel;
+
+    public void ToggleDeliveryMenu(){
+        deliveryPanel.SetActive(!deliveryPanel.activeSelf);
     }
 }
