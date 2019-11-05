81f62e7fa18534abd00435ad67b502d1e85c155f
Mary Potempa <mappotem@iu.edu>
2019-11-05 13:25:24 -0500

    plus and minus buttons add shots

--- mappotem_UI/Assets/Scripts/UIManager.cs
+++ mappotem_UI/Assets/Scripts/UIManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -8,6 +9,10 @@ public class UIManager : MonoBehaviour
     public GameObject deliveryPanel;
     public Animator menuPanel;
     public Animator drinkOptionsPanel;
+    public Animator ticketPanel;
+    public Text shotText;
+
+    private int shots = 0;
 
     public void ToggleDeliveryMenu(){
         deliveryPanel.SetActive(!deliveryPanel.activeSelf);
@@ -16,6 +21,7 @@ public class UIManager : MonoBehaviour
     public void ShowOptions()
     {
         menuPanel.SetBool("isHidden", true);
+        ticketPanel.SetBool("isHidden", true);
         drinkOptionsPanel.SetBool("isHidden", false);
         
     }
@@ -23,6 +29,26 @@ public class UIManager : MonoBehaviour
     public void ToMain()
     {
         drinkOptionsPanel.SetBool("isHidden", true);
+        ticketPanel.SetBool("isHidden", false);
         menuPanel.SetBool("isHidden", false);
+        
+    }
+    public void CancelDrink()
+    {
+        ToMain();
+    }
+    public void AddtoOrder()
+    {
+        ToMain();
+    }
+    public void AddShot()
+    {
+        shots++;
+        shotText.text = shots.ToString();
+    }
+    public void SubShot()
+    {
+        shots--;
+        shotText.text = shots.ToString();
     }
 }
