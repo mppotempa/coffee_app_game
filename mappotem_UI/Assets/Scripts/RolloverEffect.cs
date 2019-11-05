328e4d503056d373f60ff7638f07bce57f61ab53
Mary Potempa <mappotem@iu.edu>
2019-11-05 10:23:42 -0500

    created rollover images for the delivery options menu

---
+++ mappotem_UI/Assets/Scripts/RolloverEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RolloverEffect : MonoBehaviour
+{
+
+    Image img;
+    Sprite originalSprite;
+    public Sprite rollOverSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       img = GetComponent<Image>();
+       originalSprite = img.sprite;
+    }
+
+    public void Enter()
+    {
+        img.sprite = rollOverSprite;
+    }
+
+    public void Exit(){
+        img.sprite = originalSprite;
+    }
+}
